4331bf1bd651a5cdff83a69d3043948ef2372297
Konstantin Timoshenko <konstantin.t@deovr.com>
2022-03-19 16:32:47 +0300

    refact: Move scene to components

--- src/XRApp.tsx
+++ src/XRApp.tsx
@@ -1,65 +1,10 @@
 import mergeRefs from "react-merge-refs";
-import React, { useContext, useEffect, useRef } from "react";
-import { useFrame, Canvas, useOGL, RootState } from "react-ogl/web";
+import React, { useRef } from "react";
+import { Canvas, RootState } from "react-ogl/web";
 import { XRRenderer } from "./xr/XRRenderer";
-import Layer3D from "./components/Layer3D";
-import CanvasDebugFrame from "./components/CanvasDebugFrame";
-import { BaseAppContext } from "./BaseAppContext";
-import { LinkWrap, Route } from "./components/Router";
+
 import { mergeCallbacks } from "./utils/mergeCallback";
-
-const Scene = ({ visible }: { visible?: boolean }) => {
-	const state = useOGL();
-
-	useFrame(() => {
-		(state.renderer as any as XRRenderer).gl.clearColor(1, 1, 1, 1);
-	});
-
-	const baseAppContext = useContext(BaseAppContext);
-
-	useEffect(() => {
-		baseAppContext.xrState = state;
-	}, []);
-
-	return (
-		<transform position={[0, 1.6, 0] as any} visible={visible}>
-			<LinkWrap href="/">
-				<Layer3D
-					label="grid"
-					width={0.2}
-					height={0.1}
-					position={[0, 2, 0]}
-				>
-					<CanvasDebugFrame width={100} height={50} />
-				</Layer3D>
-			</LinkWrap>
-
-			<Layer3D label="grid" width={4} height={2}>
-				<CanvasDebugFrame width={1024} height={512} />
-			</Layer3D>
-			<Layer3D
-				label="left"
-				width={1}
-				height={2}
-				position={[-2, 0, 0]}
-				anchor={[0.5, 0]}
-				rotation={[0, Math.PI / 6, 0]}
-			>
-				<CanvasDebugFrame width={256} height={512} />
-			</Layer3D>
-			<Layer3D
-				label="right"
-				width={1}
-				height={2}
-				position={[2, 0, 0]}
-				anchor={[-0.5, 0]}
-				rotation={[0, -Math.PI / 6, 0]}
-			>
-				<CanvasDebugFrame width={256} height={512} />
-			</Layer3D>
-		</transform>
-	);
-};
+import { Scene } from "./components/Scene";
 
 export interface IXRAppProps {
 	onCreated?: (state: RootState) => void;
---
+++ src/components/Scene/index.tsx
@@ -0,0 +1,60 @@
+import { useContext, useEffect } from "react";
+import { useFrame, useOGL } from "react-ogl";
+import { BaseAppContext } from "../../BaseAppContext";
+import { XRRenderer } from "../../xr/XRRenderer";
+import CanvasDebugFrame from "../CanvasDebugFrame";
+import Layer3D from "../Layer3D";
+import { LinkWrap } from "../Router";
+
+export const Scene = ({ visible }: { visible?: boolean }) => {
+	const state = useOGL();
+
+	useFrame(() => {
+		(state.renderer as any as XRRenderer).gl.clearColor(1, 1, 1, 1);
+	});
+
+	const baseAppContext = useContext(BaseAppContext);
+
+	useEffect(() => {
+		baseAppContext.xrState = state;
+	}, []);
+
+	return (
+		<transform position={[0, 1.6, 0] as any} visible={visible}>
+			<LinkWrap href="/">
+				<Layer3D
+					label="grid"
+					width={0.2}
+					height={0.1}
+					position={[0, 2, 0]}
+				>
+					<CanvasDebugFrame width={100} height={50} />
+				</Layer3D>
+			</LinkWrap>
+
+			<Layer3D label="grid" width={4} height={2}>
+				<CanvasDebugFrame width={1024} height={512} />
+			</Layer3D>
+			<Layer3D
+				label="left"
+				width={1}
+				height={2}
+				position={[-2, 0, 0]}
+				anchor={[0.5, 0]}
+				rotation={[0, Math.PI / 6, 0]}
+			>
+				<CanvasDebugFrame width={256} height={512} />
+			</Layer3D>
+			<Layer3D
+				label="right"
+				width={1}
+				height={2}
+				position={[2, 0, 0]}
+				anchor={[-0.5, 0]}
+				rotation={[0, -Math.PI / 6, 0]}
+			>
+				<CanvasDebugFrame width={256} height={512} />
+			</Layer3D>
+		</transform>
+	);
+};
